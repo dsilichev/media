09d089f92c8c36419f86175295d19f82e00701d9
Dmitry <dsilichev@gmail.com>
2023-03-29 17:30:28 +0300

    adding a pause to see Loading...

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,16 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get('http://localhost:3005/users');
 
+  await pause(1000);
+
   return response.data;
 });
+
+// DEV Only!!!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  })
+}
 
 export { fetchUsers };
