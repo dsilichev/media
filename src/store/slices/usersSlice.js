46e73fa042aae87ce0eec6fac6fe018a69fcdbce
Dmitry <dsilichev@gmail.com>
2023-03-31 16:19:22 +0300

    usersListItem component

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchUsers } from "../thunks/fetchUsers";
 import { addUser } from "../thunks/addUser";
+import { removeUser } from "../thunks/removeUser";
 
 const usersSlice = createSlice({
   name: "users",
@@ -35,7 +36,22 @@ const usersSlice = createSlice({
       state.isLoading = false;
       state.error = action.error;
     });
-  }
+
+    // removeUser
+    builder.addCase(removeUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(removeUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+
+      // FIX ME
+      console.log(action);
+    });
+    builder.addCase(removeUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
